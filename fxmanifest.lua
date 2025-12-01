efca275e246fe507113a091b8e01c8c4ce3c7a50
Walls <blumvaido@gmail.com>
2025-12-01 23:00:59 +0200

    Added fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Weapon modifier'
+author 'Walls'
+version '1.0.0'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+    'client/*.lua'
+}
+
+lua54 'yes'
